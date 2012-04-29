e0dcc7a15ecc93989b0f2a1dccedb1cb9a61525e
hranchFundi <hranchFundi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-29 14:58:34 +0000

    1.0.14 fixed spelling error in SP powered by linkback
    git-svn-id: https://plugins.svn.wordpress.org/syndicate-press/trunk@537903 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- syndicate-press-plugin.php
+++ syndicate-press-plugin.php
@@ -4,7 +4,7 @@ Plugin Name: Syndicate Press
 Plugin URI: http://www.henryranch.net/software/syndicate-press/
 Description: This plugin provides a high performance, highly configurable and easy to use news syndication aggregator which supports RSS, RDF and ATOM feeds.
 Author: HenryRanch LLC (henryranch.net)
-Version: 1.0.13
+Version: 1.0.14
 Author URI: http://henryranch.net/
 License: GPL2
 */
@@ -60,7 +60,7 @@ YOU MAY REQUEST A LICENSE TO DO SO FROM THE AUTHOR.
 */
 if (!class_exists("SyndicatePressPlugin")) {
 	class SyndicatePressPlugin {
-        var $version = "1.0.13";
+        var $version = "1.0.14";
         var $homepageURL = "http://henryranch.net/software/syndicate-press/";
         
         var $cacheDir = "/cache";
@@ -310,7 +310,7 @@ if (!class_exists("SyndicatePressPlugin")) {
                 }
                 if($configOptions['showSyndicatePressLinkback'] == 'true')
                 {
-                    $content .= '<br><br><br><br>Feed aggragation powered by <a href='.$this->homepageURL.' title="Syndicate Press: A smarter feed aggregator" target=_blank>Syndicate Press</a>.';
+                    $content .= '<br><br><br><br>Feed aggregation powered by <a href='.$this->homepageURL.' title="Syndicate Press: A smarter feed aggregator" target=_blank>Syndicate Press</a>.';
                 }
                 //print "writing content to output cache file: $outputCacheFilename<br>";
                 $this->sp_writeFile($outputCacheFilename, $content);
